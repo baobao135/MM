9b86e8ce59194a153f52ececbda4aafc68e6da53
baobao135 <baobao.bui135@gmail.com>
2024-11-23 17:41:22 +0700

    v1

--- BLF_Heuristic.py
+++ BLF_Heuristic.py
@@ -23,7 +23,9 @@ class BLF:
         for _ in range(shape.demand):
             x, y = self.find_position(shape)
             if x is not None and y is not None:
-                new_shape = Rectangle(x, y, 1)
+                new_shape = Rectangle(shape.width, shape.height, 1)
+                new_shape.x = x
+                new_shape.y = y
                 self.stock.append(new_shape)
             else:
                 return False
@@ -31,21 +33,25 @@ class BLF:
     def find_position(self, shape):
         best_x, best_y = None, None
         for position in self.stock:
-            temp=[(position.x + shape.width, shape.height),(shape.x, position.y + shape.height)]
+            temp=[(position.x + shape.width, position.y),(position.x, position.y + shape.height)]
             for (x, y) in temp:
+                print(x, y)
                 if self.can_place(shape, x, y):
-                    best_x, best_y = x, y
-                    
-        if not self.stock and self.can_place(shape, 0, 0):
-            best_x, best_y = 0, 0
+                    if best_x is None or x > best_x:
+                        best_x = x
+                    if best_y is None or y > best_y:
+                        best_y = y
+                        
+        if not self.stock:
+            best_x, best_y = shape.width, shape.height
 
         return best_x, best_y
     def can_place (self, shape, width, height):
-        if shape.x + width > self.stock_w or shape.y + height > self.stock_h:
+        if shape.width + width > self.stock_w or shape.height + height > self.stock_h:
             return False
         else:
             for position in self.stock:
-                if shape.x + width < position.x and shape.y + height < position.y:
+                if shape.width + width < position.x and shape.height + height < position.y:
                     return False
         return True
     
@@ -61,22 +67,14 @@ if __name__ == "__main__":
     for width, height, demand in zip(width, height, demand):
         shapes.append(Rectangle( width, height, demand))
     shapes= Rectangle.sortShape(shapes)
-    flag = 1
     for temp in shapes:
-        for i in range(len(shapes)):
-            if shapes[i].demand == 0:
-                flag =0
-                break
         if not blf.place_shape(temp):
-            for temp1 in shapes[1:]:
+            for temp1 in shapes:
                 blf.place_shape(temp1)
             blf.stock_h = 100
             blf.stock_w = 100
             blf.stock = []
             blf.stock_idx += 1
-        else:
-            if flag:
-                blf.place_shape(temp)
         
     print ("stock_index: ", blf.stock_idx)
     
