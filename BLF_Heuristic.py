91c11c29a337d0a19e95a94cebbce53c0fa09274
baobao135 <baobao.bui135@gmail.com>
2024-11-24 17:05:23 +0700

    v3

--- BLF_Heuristic.py
+++ BLF_Heuristic.py
@@ -19,15 +19,14 @@ class BLF:
         self.stock_idx = -1
         self.stock = []
     def place_shape (self, shape):
-        for _ in range(shape.demand):
-            x, y = self.find_position(shape)
-            if x is not None and y is not None:
-                new_shape = Rectangle(shape.width, shape.height, 0)
-                new_shape.x = x
-                new_shape.y = y
-                self.stock.append(new_shape)
-            else:
-                return False
+        x, y = self.find_position(shape)
+        if x is not None and y is not None:
+            new_shape = Rectangle(shape.width, shape.height, 0)
+            new_shape.x = x
+            new_shape.y = y
+            self.stock.append(new_shape)
+        else:
+            return False
         return True
     def find_position(self, shape):
         best_x, best_y = None, None
@@ -50,10 +49,12 @@ class BLF:
                 if shape.width + x <= position.x + position.width and shape.height + y <= position.y + position.height:
                     return False
         return True
-    def display_stock (self):
+    def display_stock (self,):
         for temp in range(len(self.stock)):
-            print (temp, "shape:", self.stock[temp].width, self.stock[temp].height, self.stock[temp].x, self.stock[temp].y)
-    
+            print ("shape:", self.stock[temp].width, self.stock[temp].height, self.stock[temp].x, self.stock[temp].y)
+            
+            
+        
 if __name__ == "__main__":
     with open('input.txt', 'r') as file:
         width = [int(x) for x in file.readline().strip().split(' ')]
@@ -65,12 +66,19 @@ if __name__ == "__main__":
         shapes.append(Rectangle( width, height, demand))
     shapes= Rectangle.sortShape(shapes)
     print(shapes)
+    count = 0
     for temp in shapes:
-        if not blf.place_shape(temp):
-            blf.stock_h = 100
-            blf.stock_w = 100
-            blf.display_stock()
-            blf.stock = []
-        
+        count = 0
+        while temp.demand != 0:
+            if not blf.place_shape(temp):
+                blf.stock_h = 100
+                blf.stock_w = 100
+                blf.display_stock()
+                blf.stock = []
+                blf.stock_idx += 1
+            temp.demand = temp.demand - 1
+            count += 1
+        print(count)
+    print("Total stock: ", blf.stock_idx)    
     
     
