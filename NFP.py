be0a9b5e2bedb91e7efb58fd15476ad0b48ac04c
baobao135 <baobao.bui135@gmail.com>
2024-12-15 17:05:27 +0700

    v8

--- NFP.py
+++ NFP.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+
 class Rectangle:
     def __init__(self, width, height, demand):
         self.width = width
@@ -8,7 +9,7 @@ class Rectangle:
         self.demand = demand
         self.x = 0
         self.y = 0
-        
+    
     def __repr__(self):
         return f"Rectangle({self.width}x{self.height}, demand={self.demand}, position=({self.x}, {self.y}))"
 
@@ -17,14 +18,15 @@ class Rectangle:
         shape.sort(key=lambda temp: temp.width * temp.height, reverse=True)
         return shape
 
-class BLF:
+class NFP:
     def __init__(self, stock_w, stock_h):
         self.stock_w = stock_w
         self.stock_h = stock_h
-        self.stock_idx = 0
         self.stock = []
-        self.waste = self.stock_w * self.stock_h
-    def place_shape (self, shape):
+        self.stock_count = 0  # Biến đếm số lượng không gian đã sử dụng
+        self.total_waste = stock_w * stock_h
+
+    def place_shape(self, shape):
         x, y = self.find_position(shape)
         if x is not None and y is not None:
             new_shape = Rectangle(shape.width, shape.height, 0)
@@ -34,44 +36,46 @@ class BLF:
         else:
             return False
         return True
+
     def find_position(self, shape):
+        best_position = None
         best_x, best_y = None, None
-        for position in self.stock:
-            temp=[(position.x + position.width, position.y),(position.x, position.y + position.height)]
-            for (x, y) in temp:
+
+        # Duyệt qua tất cả các vị trí khả thi
+        for x in range(self.stock_w - shape.width + 1):
+            for y in range(self.stock_h - shape.height + 1):
                 if self.can_place(shape, x, y):
                     if best_y is None or (y < best_y) or (y == best_y and x < best_x):
-                        best_x, best_y = x, y 
-                   
+                        best_x, best_y = x, y
         if not self.stock and self.can_place(shape, 0, 0):
             best_x, best_y = 0, 0
-
         return best_x, best_y
-    def can_place (self, shape, x, y):
-        if shape.width + x > self.stock_w or shape.height + y > self.stock_h:
+
+    def can_place(self, shape, x, y):
+        if x + shape.width > self.stock_w or y + shape.height > self.stock_h:
             return False
-        else:
-            for position in self.stock:
-                if  x < position.x + position.width and y < position.y + position.height:
-                    return False
+        for placed_shape in self.stock:
+            if not (x + shape.width <= placed_shape.x or x >= placed_shape.x + placed_shape.width or
+                    y + shape.height <= placed_shape.y or y >= placed_shape.y + placed_shape.height):
+                return False
         return True
-    
-    def display_stock (self):
+
+    def display_stock(self):
         for temp in range(len(self.stock)):
-            print ("shape:", self.stock[temp].width, self.stock[temp].height, self.stock[temp].x, self.stock[temp].y)
-    
+            print("shape:", self.stock[temp].width, self.stock[temp].height, self.stock[temp].x, self.stock[temp].y)
+
     def draw_stock(self):
         fig, ax = plt.subplots()
         ax.add_patch(plt.Rectangle((0, 0), self.stock_w, self.stock_h, edgecolor='black', facecolor='lightgrey'))
 
         color_map = {}
-        
+
         for shape in self.stock:
             size_key = (shape.width, shape.height)
             if size_key not in color_map:
                 color_map[size_key] = np.random.rand(3,)
             color = color_map[size_key]
-            
+
             ax.add_patch(plt.Rectangle((shape.x, shape.y), shape.width, shape.height, edgecolor='black', facecolor=color, lw=2))
             ax.text(shape.x + shape.width / 2, shape.y + shape.height / 2, 
                     f'{shape.width}x{shape.height}', 
@@ -84,54 +88,54 @@ class BLF:
         plt.xlabel('Width')
         plt.ylabel('Height')
 
-        plt.show()        
+        plt.show()
 
-       
-        
+    def update_stock_count(self):
+        self.stock_count += 1  # Tăng số lượng không gian khi không thể đặt thêm hình vào không gian hiện tại
 if __name__ == "__main__":
     with open('input.txt', 'r') as file:
         width = [int(x) for x in file.readline().strip().split(' ')]
         height = [int(x) for x in file.readline().strip().split(' ')]
         demand = [int(x) for x in file.readline().strip().split(' ')]
-    blf = BLF(100, 100)    
+    nfp = NFP(100, 100)
     shapes = []
     for width, height, demand in zip(width, height, demand):
+
         start_time = time.time()
-        shapes.append(Rectangle( width, height, demand))
-    shapes= Rectangle.sortShape(shapes)
+        shapes.append(Rectangle(width, height, demand))
+    shapes = Rectangle.sortShape(shapes)
     count = 0
     for temp in shapes:
         count = temp.demand
         while temp.demand > 0:
-            if not blf.place_shape(temp):
+            if not nfp.place_shape(temp):
                 for temp1 in shapes:
                     while temp1.demand > 0:
-                        if not blf.place_shape(temp1):
+                        if not nfp.place_shape(temp1):
                             break
                         else:
-                            blf.waste = blf.waste - (temp1.width * temp1.height)
-                            temp1.demand = temp1.demand - 1
-                blf.stock_h = 100
-                blf.stock_w = 100
-                #blf.display_stock()
-                # blf.draw_stock()
-                blf.stock = []
-                blf.waste=blf.waste + blf.stock_w*blf.stock_h
-                blf.stock_idx += 1
+                            nfp.total_waste  = nfp.total_waste - (temp1.width * temp1.height)
+                            temp1.demand -= 1
+                nfp.update_stock_count()
+                nfp.stock_h = 100
+                nfp.stock_w = 100
+                #nfp.display_stock()
+                #nfp.draw_stock()
+                nfp.stock = []
+                nfp.total_waste = nfp.total_waste + nfp.stock_w * nfp.stock_h 
             else:
-                blf.waste = blf.waste - (temp.width * temp.height)
-                temp.demand = temp.demand - 1
-                #print(count-temp.demand)
-    # if(len(blf.stock)!=0):
-    #     #blf.display_stock()
-    #     blf.draw_stock()
-    print("Total stock: ", blf.stock_idx + 1)
-    print("Total area: ",(blf.stock_idx + 1)*100*100) 
-    print("Total waste: ", blf.waste, "percentage: ", blf.waste/((blf.stock_idx + 1)*100*100))
+                nfp.total_waste = nfp.total_waste - (temp.width * temp.height)
+                temp.demand -= 1
+                #print(count - temp.demand)
+    # if len(nfp.stock) != 0:
+    #     #nfp.display_stock()
+    #     nfp.draw_stock()
+    print("Total stock: ", nfp.stock_count + 1)
+    print("Total area: ", (nfp.stock_count + 1)*100*100)
+    print("Total waste: ", nfp.total_waste, "percentage: ", nfp.total_waste / ((nfp.stock_count + 1) * 100 * 100))
     # Kết thúc đo thời gian
     end_time = time.time()
 
     # Tính toán và in ra thời gian thực thi
     execution_time = end_time - start_time
     print("Execution time: ", execution_time, "seconds")
-   
